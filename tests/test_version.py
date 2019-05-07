679392d3e3b58bf162b580db44100c65231a4583
Peter Bittner <django@bittner.it>
2019-05-07 14:10:40 +0200

    Mock CLI arguments for running the tests

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,10 +1,18 @@
 """
-Tests for filtering by Python version 
+Tests for filtering by Python version
 """
-import os
 import platform
 import pytest
 import sys
+
+try:
+    from unittest.mock import patch
+except ImportError:  # Python 2.7, PyPy2
+    from mock import patch
+
+import pyclean.cli
+
+from helpers import ArgvContext
 
 
 @pytest.mark.skipif(sys.version_info >= (3,), reason="requires Python 2")
@@ -12,8 +20,8 @@ def test_filterversion_py2():
     """
     Does filtering by Python version work when run with Python 2?
     """
-    exit_status = os.system('pyclean -V 2.7 -p python-apt')
-    assert exit_status == 0
+    with ArgvContext('-V', '2.7', '-p', 'python-apt'):
+        pyclean.cli.pyclean()
 
 
 @pytest.mark.skipif(sys.version_info < (3,), reason="requires Python 3")
@@ -21,27 +29,33 @@ def test_filterversion_py3():
     """
     Does filtering by Python version work when run with Python 3?
     """
-    exit_status = os.system('py3clean -V 3.5 -p python-apt')
-    assert exit_status == 0
+    with ArgvContext('-V', '3.5', '-p', 'python-apt'):
+        pyclean.cli.py3clean()
 
 
 @pytest.mark.skipif(platform.python_implementation() != 'PyPy'
                     or sys.version_info >= (3,),
                     reason="requires PyPy2")
-def test_filterversion_pypy():
+@patch('pyclean.pypyclean.installed_namespaces', return_value={})
+def test_filterversion_pypy(mock_namespaces):
     """
     Does filtering by Python version work when run with PyPy?
     """
-    exit_status = os.system('pypyclean -V 2.7 -p python-apt')
-    assert exit_status == 0
+    with ArgvContext('-V', '2.7', '-p', 'python-apt'):
+        pyclean.cli.pypyclean()
+
+    assert mock_namespaces.called
 
 
 @pytest.mark.skipif(platform.python_implementation() != 'PyPy'
                     or sys.version_info < (3,),
                     reason="requires PyPy3")
-def test_filterversion_pypy3():
+@patch('pyclean.pypyclean.installed_namespaces', return_value={})
+def test_filterversion_pypy3(mock_namespaces):
     """
     Does filtering by Python version work when run with PyPy3?
     """
-    exit_status = os.system('pypyclean -V 3.5 -p python-apt')
-    assert exit_status == 0
+    with ArgvContext('-V', '3.5', '-p', 'python-apt'):
+        pyclean.cli.pypyclean()
+
+    assert mock_namespaces.called
