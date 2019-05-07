56eefef7881a4f45f836202a49fce33a8720dd1f
Peter Bittner <django@bittner.it>
2019-05-07 16:06:09 +0200

    Bump version to dev

--- pyclean/__init__.py
+++ pyclean/__init__.py
@@ -4,5 +4,5 @@ Pure Python cross-platform pyclean. Clean up your Python bytecode.
 __author__ = 'Peter Bittner'
 __email__ = 'django@bittner.it'
 __url__ = 'https://github.com/bittner/pyclean'
-__version__ = '1.0.0'
+__version__ = '2.0.0dev0'
 __license__ = 'GPLv3'
