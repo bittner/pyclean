3ed19ea62fe14fd1ce88f78402be0446cb01228c
Peter Bittner <django@bittner.it>
2020-04-13 17:55:38 +0200

    Add explanation to exception to avoid issues being reported

--- pyclean/modern.py
+++ pyclean/modern.py
@@ -1,4 +1,3 @@
-
 """
 Modern, cross-platform, pure-Python pyclean implementation.
 """
@@ -6,4 +5,5 @@ Modern, cross-platform, pure-Python pyclean implementation.
 
 def pyclean(args):
     """Cross-platform cleaning of Python bytecode"""
-    raise NotImplementedError()
+    raise NotImplementedError(
+        "Use the --legacy option, or the py2clean or py3clean aliases")
