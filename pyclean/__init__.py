6874263d06f2b7bd4b602163d787ece15566d7ee
Peter Bittner <django@bittner.it>
2022-03-06 16:19:01 +0100

    Release v2.1.0
    This release is mainly for updating the metadata on PyPI.
    No significant code changes were made.
    
    Changes: https://github.com/bittner/pyclean/compare/2.0.0..2.1.0

--- pyclean/__init__.py
+++ pyclean/__init__.py
@@ -4,4 +4,4 @@ Pure Python cross-platform pyclean. Clean up your Python bytecode.
 __author__ = 'Peter Bittner'
 __email__ = 'django@bittner.it'
 __url__ = 'https://github.com/bittner/pyclean'
-__version__ = '2.0.0'
+__version__ = '2.1.0'
