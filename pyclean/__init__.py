ffd6ac1530db8ed7df8720cba383a1991376be31
Peter Bittner <django@bittner.it>
2022-03-06 15:56:06 +0100

    Remove license key as suggested by Python Packaging docs
    See https://packaging.python.org/en/latest/guides/distributing-packages-using-setuptools/#license

--- pyclean/__init__.py
+++ pyclean/__init__.py
@@ -3,6 +3,5 @@ Pure Python cross-platform pyclean. Clean up your Python bytecode.
 """
 __author__ = 'Peter Bittner'
 __email__ = 'django@bittner.it'
-__license__ = 'GPLv3'
 __url__ = 'https://github.com/bittner/pyclean'
 __version__ = '2.0.0'
